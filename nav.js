952caa38f4ae88c1c9c873a657a093b1e622609a
bandorigall <bandorigall@gmail.com>
2026-08-01 13:52:14 +0900

    nav.js에 캐릭터소터 메뉴 추가

--- nav.js
+++ nav.js
@@ -17,7 +17,8 @@
     ['캐릭터프로필', 'https://bandorigall.github.io/others.github.io/our_notes_profile/'],
     ['김치쿠라',    'https://bandorigall.github.io/others.github.io/kimchikura/'],
     ['마이고라이브', 'https://bandorigall.github.io/others.github.io/mygo_live/'],
-    ['기타도감',    'https://bandorigall.github.io/others.github.io/guitar/']
+    ['기타도감',    'https://bandorigall.github.io/others.github.io/guitar/'],
+    ['캐릭터소터',  'https://bandorigall.github.io/others.github.io/sorter/']
   ];
 
   var curPath = location.pathname.replace(/index\.html$/, '');
